e3f12804d2fba0bd89bcbefe59efe0aaa8f0d20c
Saad Meer <saad.wicks@gmail.com>
2022-09-02 22:58:17 +0500

    second commit

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const product = ({ product }) => {
   return (
     <div className="col-md-3 m-5 p-3 border">
       <div>
-        {/* <Link to={`product/${product.id}`}> */}
+        <Link to={`product/${product.id}`} exact="true" >
           <img className="img-fluid" src={product.image} alt="" />
           <h1>{product.name}</h1>
           <h1>Rating : {product.rating}</h1>
           <h1>Price : {product.price}</h1>
-        {/* </Link> */}
+        </Link>
       </div>
     </div>
   );
--- src/screens/Productdescscreen.jsx
+++ src/screens/Productdescscreen.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
+import products from "../products";
 
-const Productdescscreen = () => {
+export default function Productdescscreen() {
+
+  const { id } = useParams();
+
+  const product = products.find(product => product.id == id);
+  // const productid = match.params.id;
+
   return (
     <div>
-      <h1>Product Description</h1>
+      
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card p-5 m-2">
+            <h1>{product.name}</h1>
+            <img src={product.image} className="img-fluid product_img" alt="product" />
+            <p>{product.description}</p>
+          </div>
+        </div>
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default Productdescscreen;
+// export default Productdescscreen;
